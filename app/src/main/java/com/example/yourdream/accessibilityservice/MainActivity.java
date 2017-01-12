c4f3d05582afeb4978791f4f92519d629510ae3d
xiaobing.liu <xiaobing.liu@yourdream.cc>
2017-01-12 13:57:02 +0800

    init commit

--- app/src/main/java/com/example/yourdream/accessibilityservice/MainActivity.java
+++ app/src/main/java/com/example/yourdream/accessibilityservice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.yourdream.accessibilityservice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startPlugin(View view){
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
 
     }
 }
